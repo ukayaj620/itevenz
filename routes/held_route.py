9a50ad1f8670911e58f6e8a8361440726af7a05f
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2021-04-03 13:19:53 +0700

    feat: update route

--- routes/held_route.py
+++ routes/held_route.py
@@ -1,10 +1,7 @@
-from flask import Blueprint, url_for, render_template, request, abort
+from flask import Blueprint, url_for, render_template, request
 from ..controllers.event_controller import EventController
 from flask_login import login_required, current_user
-from ..utils.images import validate_image, generate_photoname
-from werkzeug.utils import secure_filename
-from ..config import Config
-import os
+from ..utils.images import save_image
 
 held = Blueprint('held', __name__, template_folder='templates')
 
@@ -33,15 +30,7 @@ def create():
     speaker = request.form.get('speaker')
     photo = request.files['photo']
 
-    filename = secure_filename(photo.filename)
-    photo_filename = ''
-    if filename != '':
-      file_ext = os.path.splitext(filename)[1].lower()
-      photo_filename = generate_photoname(file_ext)
-      if file_ext not in Config.UPLOAD_EXTENSIONS or \
-          file_ext != validate_image(photo.stream):
-        abort(400)
-      photo.save(os.path.join(Config.UPLOAD_PATH, photo_filename))
+    photo_filename = save_image(photo)
 
     return EventController.create(
       EventController,
@@ -64,7 +53,39 @@ def create():
 @login_required
 def update(id):
   if request.method == 'POST':
-    return ""
+    id = request.form.get('id')
+    title = request.form.get('title')
+    description = request.form.get('description')
+    due_date = request.form.get('due-date')
+    category = request.form.get('category')
+    start_date = request.form.get('start-date')
+    start_time = request.form.get('start-time')
+    end_date = request.form.get('end-date')
+    end_time = request.form.get('end-time')
+    speaker = request.form.get('speaker')
+    photo = request.files['photo']
+
+    photo_filename = save_image(photo) if photo else None
+
+    return EventController.update(
+      EventController,
+      title=title,
+      description=description,
+      start_date=start_date,
+      end_date=end_date,
+      category=category,
+      start_time=start_time,
+      end_time=end_time,
+      due_date=due_date,
+      poster_filename=photo_filename,
+      speaker=speaker,
+      event_id=id
+    )
 
   return render_template('/held/update.html', event=EventController.fetch_by_id(EventController, id))
 
+
+@held.route('/delete/<int:id>')
+@login_required
+def delete(id):
+  return EventController.delete(EventController, event_id=id)
