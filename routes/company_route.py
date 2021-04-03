86956b2deee4e29107a1527c11f2b965d35b4b6b
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2021-04-04 03:45:30 +0700

    feat: add new route to view event detail

--- routes/company_route.py
+++ routes/company_route.py
@@ -1,4 +1,7 @@
 from flask import Blueprint, render_template
+from ..controllers.event_controller import EventController
+from ..controllers.participation_controller import ParticipationController
+from flask_login import current_user
 
 company = Blueprint('company', __name__, template_folder='templates')
 
@@ -12,5 +15,14 @@ def about():
 
 @company.route('/event')
 def event():
-  return render_template('company/event.html')
+  return EventController.fetch_all(EventController, current_user.id if current_user.is_authenticated else None)
+
+@company.route('event/view/<int:event_id>')
+def event_detail(event_id):
+  event = EventController.fetch_by_id(EventController, event_id=event_id)
+
+  if current_user.is_authenticated:
+    participated_event = ParticipationController.fetch_participation(ParticipationController, user_id=current_user.id, event_id=event_id)
+
+  return render_template('company/event_detail.html', event=event, participated_event=participated_event)
 
